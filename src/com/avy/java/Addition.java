f0e90a955125d6a6fa9c2eb6fd5bfc4068ecb745
Avy <avy.garry@gmail.com>
2020-06-08 19:33:01 -0400

    Hello world homework

---
+++ src/com/avy/java/Addition.java
@@ -0,0 +1,3 @@
+package com.avy.java;
+
+public class Addition {	public static void main(String[] args) {	System.out.println("test");	}}
---
+++ src/com/avy/java/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.avy.java;
+
+public class HelloWorld {
+	
+	public static void main (String [] arg) {
+		System.out.print("hello betas");
+		System.out.println("");
+	}
+	
+}

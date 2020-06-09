9770b888a958b7aa1b1ca369c44eb710756c3b24
Avy <avy.garry@gmail.com>
2020-06-09 18:07:43 -0400

    Homework day 1 star

---
+++ src/com/avy/java/Star1.java
@@ -0,0 +1,12 @@
+package com.avy.java;
+
+public class Star1 {
+	public static void main (String [] arg) {
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*****");
+	}
+ 
+}

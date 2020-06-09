c3df7502f4c181a1da7761b591bbfa2ad36517d3
Avy <avy.garry@gmail.com>
2020-06-09 18:15:07 -0400

    Star2

--- src/com/avy/java/Addition.java
+++ src/com/avy/java/Addition.java
@@ -1,3 +1,7 @@
 package com.avy.java;
 
-public class Addition {	public static void main(String[] args) {	System.out.println("test");	}}
+public class Addition {
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+}
---
+++ src/com/avy/java/Star2.java
@@ -0,0 +1,12 @@
+package com.avy.java;
+
+public class Star2 {
+	public static void main (String [] arg) {
+		System.out.println("    *  ");
+		System.out.println("   * * ");
+		System.out.println("  * * *");
+		System.out.println(" * * * *");
+		System.out.println("* * * * * ");
+	}
+ 
+}

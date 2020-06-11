cf87385dbddfcfceb865ff4af4a103500fcc9c6d
Avy <avy.garry@gmail.com>
2020-06-11 15:32:36 -0400

    no message

---
+++ src/com/avy/java/Class3Homework1.java
@@ -0,0 +1,26 @@
+package com.avy.java;
+
+public class Class3Homework1 {
+	public static void main (String [] arg) {
+		byte GB = 127;
+		//The lowest is -128, and the highest is 127
+		short TINY = 562;
+		//The lowest is -32,768 and the higest is 32,767
+		int ABCDEF = 65981;
+		//The lowest is -2,147,483,648 and the higest is 2,147,483,647
+		long WOAH = 83143;
+		//The lowest is -9,223,372,036,854,775,808 and the highest is 9,223,372,036,854,775,807
+		float PI = 3.14f;
+		//N/A
+		double ANOTHERPI = 3.14;
+		//N/A
+		boolean TrueOrFalse = false;
+		//The lowest is false, and the higest is true
+		char DEFG = 'w';
+		//The lowest is 0, and the higest 65,535
+		
+		
+		
+	}
+
+}

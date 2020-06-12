4dbdf44ea0d7908de969fc8cc132a580da5b9a7f
Avy <avy.garry@gmail.com>
2020-06-11 23:31:09 -0400

    Homework for uhhhhh

---
+++ src/com/avy/java/chapter3/Chapter3Homework4LineMultadd.java
@@ -0,0 +1,23 @@
+package com.avy.java.chapter3;
+
+import java.util.Scanner;
+
+public class Chapter3Homework4LineMultadd  {
+	public static float readUserFloatInput (String aInputMsg) {
+		System.out.print( aInputMsg + " ");
+		Scanner scanner = new Scanner(System.in);
+		return scanner.nextFloat();
+	
+	}
+	
+	
+	public static void main (String[] arg) {
+		float num1 = readUserFloatInput("Enter the first number: ");
+		float num2 = readUserFloatInput("Enter the second number: ");
+		float num3 = readUserFloatInput("Enter the third number: ");
+		float answer = num1 + num2 * num3;
+		
+		System.out.println(num1 + " + " + num2 + " x " + num3 + " = " + answer);
+
+	}
+}
---
+++ src/com/avy/java/chapter3/Chapter3Homework4Subtraction.java
@@ -0,0 +1,22 @@
+package com.avy.java.chapter3;
+
+import java.util.Scanner;
+
+public class Chapter3Homework4Subtraction {
+	public static float readUserFloatInput (String aInputMsg) {
+		System.out.print( aInputMsg + " ");
+		Scanner scanner = new Scanner(System.in);
+		return scanner.nextFloat();
+	
+	}
+	
+	
+	public static void main (String[] arg) {
+		float num1 = readUserFloatInput("Enter the first number: ");
+		float num2 = readUserFloatInput("Enter the second number: ");
+		float answer = num1 - num2;
+		
+		System.out.println(num1 + " - " + num2 + " = " + answer);
+
+	}
+}
